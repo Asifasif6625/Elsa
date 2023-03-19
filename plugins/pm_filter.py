57a390eb0bdc2eac1180d6ec300c4b2780ffcb0c
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2023-03-19 11:17:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1174,6 +1174,14 @@ async def auto_filter(client, msg, spoll=False):
             search = message.text
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files:
+                k = await msg.reply_photo(
+                photo=SPELL_IMG, 
+                caption=script.I_CUDNT.format(mv_rqst),
+                reply_markup=InlineKeyboardMarkup(button)
+                )
+                await asyncio.sleep(30)
+                await k.delete()
+                return
                 buttons = [[ InlineKeyboardButton(f' ᴍꜱɢ ɪɴ ᴍᴀʟᴀyᴀʟᴀᴍ ', 'makri') ]]
                 z = await message.reply_text(text=f"<i>{message.from_user.mention}🪛I don't have a file in the word you sent, sometimes the word you sent is wrong.If this movie is an OTT release please check the spelling and then send it.</i>", reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(100)

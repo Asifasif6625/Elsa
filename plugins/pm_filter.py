54b612d2394b251d85f049de907f27cc91844551
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2023-03-20 10:56:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1175,7 +1175,7 @@ async def auto_filter(client, msg, spoll=False):
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files:
                 buttons = [[ InlineKeyboardButton(f' ᴍꜱɢ ɪɴ ᴍᴀʟᴀyᴀʟᴀᴍ ', 'makri') ]]
-                z = await message.reply_video(video=NOR_IMG, text=f"<i>{message.from_user.mention}🪛I don't have a file in the word you sent, sometimes the word you sent is wrong.If this movie is an OTT release please check the spelling and then send it.</i>", reply_markup=InlineKeyboardMarkup(buttons))
+                z = await message.reply_text(text=f"<i>{message.from_user.mention}🪛I don't have a file in the word you sent, sometimes the word you sent is wrong.If this movie is an OTT release please check the spelling and then send it.</i>", reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(100)
                 await z.delete()
                 if settings["spell_check"]:

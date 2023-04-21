62d316fce163aaa0c3dbb84a698dddac413c9759
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2023-04-21 17:44:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -230,6 +230,8 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
+    await message.reply_chat_action(enums.ChatAction.TYPING)
+    await asyncio.sleep(5)
     await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,

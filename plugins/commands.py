2e86fe79186f4b8ac7a97fa98aa93bac51dd494f
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2023-04-22 00:51:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -233,7 +233,7 @@ async def start(client, message):
     buttons = [[ InlineKeyboardButton(f' Ok,Get File🎞️ ', callback_data="next12") ]] 
     xz = await message.reply_text(f"-ꜰɪʟᴇ ᴅᴇᴛᴀɪʟꜱ-\n\n•ꜰɪʟᴇ ɴᴀᴍᴇ - {files.file_name}\n•ꜰɪʟᴇ ꜱɪᴢᴇ - {files.file_size}\n\n• ഈ ഫയൽ 10 മിനിറ്റ് കഴിയുമ്പോൾ ഓട്ടോമാറ്റിക് ആയി ഡിലീറ്റ് ആയി പോകും അതിനാൽ മറ്റവിടെയെങ്കിലും ഫോർവേഡ് ചെയ്ത ശേഷം ഡൌൺലോഡ് ചെയ്യുക.", reply_markup=InlineKeyboardMarkup(buttons))
     if query.data == "next12":
-        await xz.delete()
+        await query.answer(close_data)
     await message.reply_chat_action(enums.ChatAction.UPLOAD_DOCUMENT)
     await asyncio.sleep(5)
     await client.send_cached_media(

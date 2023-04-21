39cc934753b836781a259e90e8f3d7a055ededf9
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2023-04-22 00:08:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -232,18 +232,20 @@ async def start(client, message):
         f_caption = f"{files.file_name}"
     buttons = [[ InlineKeyboardButton(f' Ok,Get File🎞️ ', callback_data="next12") ]] 
     xz = await message.reply_text(f"-ꜰɪʟᴇ ᴅᴇᴛᴀɪʟꜱ-\n\n•ꜰɪʟᴇ ɴᴀᴍᴇ - {files.file_name}\n•ꜰɪʟᴇ ꜱɪᴢᴇ - {files.file_size}\n\n• ഈ ഫയൽ 10 മിനിറ്റ് കഴിയുമ്പോൾ ഓട്ടോമാറ്റിക് ആയി ഡിലീറ്റ് ആയി പോകും അതിനാൽ മറ്റവിടെയെങ്കിലും ഫോർവേഡ് ചെയ്ത ശേഷം ഡൌൺലോഡ് ചെയ്യുക.", reply_markup=InlineKeyboardMarkup(buttons))
-    await message.reply_chat_action(enums.ChatAction.UPLOAD_DOCUMENT)
-    await asyncio.sleep(5)
-    await client.send_cached_media(
-        chat_id=message.from_user.id,
-        file_id=file_id,
-        caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('❤️‍🔥 ᴊᴏɪɴ ᴛᴏ ᴄʜᴀɴɴᴇʟ ❤️‍🔥', url=(MAIN_CHANNEL)) ] ] ),
-        protect_content=True if pre == 'filep' else False,
-        )
-   
     elif query.data == "next12":
-        await xz.delete()                
+       await message.reply_chat_action(enums.ChatAction.UPLOAD_DOCUMENT)
+       await asyncio.sleep(5)
+       await xz.delete()
+       await client.send_cached_media(
+           chat_id=message.from_user.id,
+           file_id=file_id,
+           caption=f_caption,
+           reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('❤️‍🔥 ᴊᴏɪɴ ᴛᴏ ᴄʜᴀɴɴᴇʟ ❤️‍🔥', url=(MAIN_CHANNEL)) ] ] ),
+           protect_content=True if pre == 'filep' else False,
+           )
+   
+    
+                        
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):

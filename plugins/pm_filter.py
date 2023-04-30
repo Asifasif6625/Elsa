af56c527dbc78db217c7792306192b07a2db67b1
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2023-04-30 15:54:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1211,7 +1211,7 @@ async def auto_filter(client, msg, spoll=False):
             btn = [
                 [
                     InlineKeyboardButton(
-                        text=f"𓆏{get_size(file.file_size)}﹅ {file.file_name}", url=await get_shortlink(message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=XD-{file.file_id}")
+                        text=f"𓆏{get_size(file.file_size)}﹅ {file.file_name}", url=await get_shortlink(message.chat.id, f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
                     ),
                 ]
                 for file in files
